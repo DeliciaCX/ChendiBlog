e2b709dc4a7c7a77b38fb7585d38d17648e21ad8
Delicia <cx767@uowmail.edu.au>
2022-07-28 17:24:04 +1000

    Intro page + typeing effect is so difficult to add

--- chendiblog/src/components/intro/Intro.jsx
+++ chendiblog/src/components/intro/Intro.jsx
@@ -1,9 +1,44 @@
 import "./intro.scss";
+import Typewriter from 'typewriter-effect';
 
 export default function Intro() {
+
     return (
         <div className="intro" id="intro">
-           Intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt=""/>
+                </div>
+            </div>
+
+
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Chendi Xie</h1>
+                    <h3>
+                        Full time <span>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter.typeString('Information Technology Student')
+                                    .callFunction(() => {
+                                        console.log('String typed out!');
+                                    })
+                                    .pauseFor(2500)
+                                    .deleteAll()
+                                    .callFunction(() => {
+                                        console.log('All strings were deleted');
+                                    })
+                                    .start();
+                            }}
+                        />
+                        </span>
+                    </h3>
+                </div>
+                <a href="#blog">
+                    <img src="assets/down.png" alt=""/>
+                </a>
+            </div>
         </div>
     );
 }
